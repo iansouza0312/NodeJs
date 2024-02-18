ee040dfebcb939448a91d2f37648a4c13e4023c5
Ian Souza <ianfmsouza@gmail.com>
2024-02-17 22:09:00 -0300

    Add Routing

--- basic-concept/app.js
+++ basic-concept/app.js
@@ -1,12 +1,20 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  // process.exit();
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write("<body>this is a new page of webserver</body>");
+    res.write("</html>");
+    return res.end();
+  }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><p>this is a server response</p></body>");
+  res.write("<head><title>UNCAUGHT PAGE</title></head>");
+  res.write("<body>error 404</body>");
   res.write("</html>");
   res.end();
 });
